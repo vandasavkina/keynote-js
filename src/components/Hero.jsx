b8c81a656465c77affd3b914bce2643d5b09d053
vandasavkina <wandasavkina@hotmail.it>
2024-04-01 19:57:43 +0200

    Changes to be committed: modified:   src/components/Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,8 +9,9 @@ export function Hero() {
       <Container className="relative">
         <div className="mx-auto max-w-2xl lg:max-w-4xl lg:px-12">
           <h1 className="font-display text-5xl font-bold tracking-tighter text-blue-600 sm:text-7xl">
-            <span className="sr-only">DeceptiConf - </span>A design conference
-            for the dark side.
+            <span className="sr-only">TechResearch - </span>Data Storage in
+            Synthetic DNA: Navigating thecCrossroads of Technological Innovation
+            and Environmental Sustainability.
           </h1>
           <div className="mt-6 space-y-6 font-display text-2xl tracking-tight text-blue-900">
             <p>
