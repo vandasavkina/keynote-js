8e5b6b43702ff8715022f3e202173aacb10aba55
vandasavkina <wandasavkina@hotmail.it>
2024-04-01 20:59:01 +0200

    Changes to be committed: modified:   src/components/Hero.jsx modified:   src/components/Logo.jsx new file:   src/images/tech-research.png

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,13 +10,13 @@ export function Hero() {
         <div className="mx-auto max-w-2xl lg:max-w-4xl lg:px-12">
           <h1 className="font-display text-5xl font-bold tracking-tighter text-blue-600 sm:text-7xl">
             <span className="sr-only">TechResearch - </span>Data Storage in
-            Synthetic DNA.
+            Synthetic DNA
           </h1>
           <div className="mt-6 space-y-6 font-display text-2xl tracking-tight text-blue-900">
-            <p>
+            {/* <p>
               Navigating the Crossroads of Technological Innovation and
               Environmental Sustainability.
-            </p>
+            </p> */}
             <p>
               In the quest for sustainable and innovative data storage
               solutions, synthetic DNA emerges as a frontier technology with the
