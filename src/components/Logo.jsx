eb4a93fab1f57772b0dec8fb86eaa074fe960366
vandasavkina <wandasavkina@hotmail.it>
2024-04-02 21:03:23 +0200

    Changes to be committed: 	modified:   src/components/Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,9 +2,9 @@ export function Logo(props) {
   return (
     <svg width="500" height="100" viewBox="0 0 200 100" {...props}>
       {/* Diagonal Line 1 */}
-      <line x1="10" y1="10" x2="50" y2="50" stroke="#3B82F6" strokeWidth="4" />
+      <line x1="10" y1="10" x2="50" y2="50" stroke="#3B82F6" strokeWidth="6" />
       {/* Diagonal Line 2 */}
-      <line x1="20" y1="10" x2="60" y2="50" stroke="#93C5FD" strokeWidth="4" />
+      <line x1="25" y1="10" x2="65" y2="50" stroke="#93C5FD" strokeWidth="6" />
       {/* Text "TechResearch" */}
       <text
         x="70"
@@ -13,7 +13,7 @@ export function Logo(props) {
         fontSize="40"
         fill="#0F172A"
       >
-        TechResearch
+        VandaVision()
       </text>
     </svg>
   )
